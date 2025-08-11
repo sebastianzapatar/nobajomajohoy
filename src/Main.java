65e3c52487284612afe514222b8e1e4bed711b07
Alejandro Urrego <urrexx@Alejandros-MacBook-Pro.local>
2025-08-11 11:26:59 -0500

    majo estuvo aki

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sera que si pasan 🙄");
         System.out.println("Sueño");
+        System.out.printlm("sosamajo estuvo aqui 100% real ❤️");
     }
 }
