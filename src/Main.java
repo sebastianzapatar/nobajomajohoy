1b96adac7c603a6fb678e34bc78db445e94c8307
unknown <jxxnpx@gmail.com>
2025-08-11 11:33:33 -0500

    la verdad sobre majo

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Sera que si pasan 🙄");
         System.out.println("Sueño");
-        System.out.printlm("sosamajo estuvo aqui 100% real ❤️");
+        System.out.println("sosamajo estuvo aqui 100% real ❤️");
+        System.out.println("Majo es re gay");
     }
 }
