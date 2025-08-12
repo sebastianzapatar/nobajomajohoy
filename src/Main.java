a66afdadfac2cd338c5297e8de71fab7daa4d85e
Andres Baena <anjubama12@gmail.com>
2025-08-11 19:50:55 -0500

    lol

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("sosamajo estuvo aqui 100% real ❤️");
         System.out.println("Majo es re gay");
         System.out.println("Alejo is here or maybe no");
-        System.out.println("Sebas godsito")
+        System.out.println("Sebas godsito");
     }
 }
