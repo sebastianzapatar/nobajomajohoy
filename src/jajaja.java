8ae87497142e33a3169e432615471e11e32ad47b
juan <juanjoseflorezlopez@gmail.com>
2025-08-13 10:06:01 -0500

    el rap de fernanflo

---
+++ src/jajaja.java
@@ -0,0 +1,90 @@
+public class jajaja {
+    public static void main(String[] args) {
+
+
+        System.out.println("¡Bambiel!\n" +
+                "\n" +
+                "Todo comienza cuando no hay nada que hacer\n" +
+                "Cuando todo lo que quieres es tener un público que entretener\n" +
+                "Cuando haces algo random\n" +
+                "Sin imaginarte que algún día el mundo de ti estará hablando\n" +
+                "\n" +
+                "Solo haces lo que te gusta\n" +
+                "Y resulta que lo que haces mucha gente lo disfruta\n" +
+                "Ingresas a YouTube y creas un canal\n" +
+                "Creas un personaje y dejas de ser normal\n" +
+                "\n" +
+                "Miles de likes, miles de vistas\n" +
+                "Viendo tu nombre en las mejores listas\n" +
+                "Miles de fans, se hacen millones\n" +
+                "Se vuelven realidad muchas de tus ilusiones\n" +
+                "\n" +
+                "Botón de plata, botón de oro\n" +
+                "Y la placa de diamante, que es el mayor tesoro\n" +
+                "Pocos lo han logrado, y de uno es que te voy a hablar yo\n" +
+                "Este es el rap de Fernanfloo\n" +
+                "\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "El rap de Fernanfloo, flo, flo, flo, flo\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "Si no te gusta, andate al chorizo\n" +
+                "\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "El rap de Fernanfloo, flo, flo, flo, flo\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "Si no te gusta, andate al carajo\n" +
+                "\n" +
+                "Si quieres ver un gameplay matado de la risa\n" +
+                "Pues mira Fernanfloo y hazlo deprisa\n" +
+                "Fernan el crack, Fernan el fail\n" +
+                "Escucha la voz suculenta del (mmmm yeah)\n" +
+                "\n" +
+                "Me sorprende hasta donde has llegado, de verdad\n" +
+                "La gente mira tu canal y dice: Oh my God!\n" +
+                "Te pelás, te pasás y a veces la regás\n" +
+                "Y es entretenido que juegues Geometry Dash\n" +
+                "\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "El rap de Fernanfloo, flo, flo, flo, flo\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "Si no te gusta, andate al chorizo\n" +
+                "\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "El rap de Fernanfloo, flo, flo, flo, flo\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "Si no te gusta, andate al carajo\n" +
+                "\n" +
+                "Con la rana rené, con tu perrito Curly\n" +
+                "Goku que no te viola y no es que yo me burle\n" +
+                "Pero me mata de la risa tu cara del meme\n" +
+                "Es rara la ocasión en la que no menciones pene\n" +
+                "\n" +
+                "Con tu gorra de Luigi, tu risa desquiciada\n" +
+                "Lentes de violador y tu sonrisa de metal\n" +
+                "Las cosas que dices cuando se te lengua la traba\n" +
+                "Tu cara de locura cuando te desesperás\n" +
+                "\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "El rap de Fernanfloo, flo, flo, flo, flo\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "Si no te gusta, andate al chorizo\n" +
+                "\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "El rap de Fernanfloo, flo, flo, flo, flo\n" +
+                "El rap de Fernanfloo, el rap de Fernanfloo\n" +
+                "Si no te gusta, andate al carajo\n" +
+                "\n" +
+                "Eres un crack, lograste lo que pocos han logrado\n" +
+                "La gente está loca, yo igual estoy alocado\n" +
+                "Por eso tus locuras me parecen divertidas\n" +
+                "Vale un chorizo si hay mentes pervertidas\n" +
+                "\n" +
+                "Todo es una locura\n" +
+                "Mi personaje también tiene una mente inmadura\n" +
+                "Te saludo desde aquí, tu país El Salvador\n" +
+                "Espero que prosperes y sigás siendo mejor");
+    }
+
+
+
+}
